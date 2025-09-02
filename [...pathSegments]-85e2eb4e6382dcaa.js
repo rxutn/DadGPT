fc7b251b06a5d4c1dd5c246d12142b422b6f913b
Anatole <github@pgp.anatole.co>
2025-09-02 23:53:22 +0200

    Delete [...pathSegments]-85e2eb4e6382dcaa.js

--- [...pathSegments]-85e2eb4e6382dcaa.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[7850],{80477:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/[...pathSegments]",function(){return u(11623)}])}},function(n){n.O(0,[800,3159,8778,8198,4250,9994,1623,9774,2888,179],function(){return n(n.s=80477)}),_N_E=n.O()}]);
